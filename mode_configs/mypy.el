b422a9f4289b6b5ef90e6a5dac8c1e117dcd26c1
Jack Scott <jack@nine78.com>
2014-03-08 02:52:10 -0500

    trying out jedi only completion

--- mode_configs/mypy.el
+++ mode_configs/mypy.el
@@ -3,49 +3,59 @@
 ;; 	   :import [ js-functions]
 ;; 	   )
 
-;; (defun electric-pair ()
-;;   "If at end of line, insert character pair without surrounding spaces.
-;;    Otherwise, just insert the typed character."
-;;   (interactive)
-;;   (if (eolp) (let (parens-require-spaces) (insert-pair)) 
-;;     (self-insert-command 1)))
-
 ;; python
 (require 'python)
 (add-to-list 'auto-mode-alist '("\\.py\\'\\|\\.wsgi\\'" . python-mode))
-(setq py-align-multiline-strings-p t)
+
+
+
+(set-list-items '(py-force-py-shell-name-p
+		  py-align-multiline-strings-p
+		  py-shell-switch-buffers-on-execute-p
+		  py-switch-buffers-on-execute-p
+		  py-smart-indentation
+		  py-tab-indent
+		  py-indent-honors-inline-comment)
+		t)
+
+
 (setq py-load-python-mode-pymacs-p nil)
-(setq py-tab-indent t)
-(setq py-indent-honors-inline-comment t)
-;; (define-key python-mode-map "\"" 'electric-pair)
-;; (define-key python-mode-map "\'" 'electric-pair)
-;; (define-key python-mode-map "(" 'electric-pair)
-;; (define-key python-mode-map "[" 'electric-pair)
-;; (define-key python-mode-map "{" 'electric-pair)
+;;(setq py-split-windows-on-execute-p nil)
+
 (setq interpreter-mode-alist
       (cons '("python" . python-mode)
             interpreter-mode-alist)
       python-mode-hook
       '(lambda () (progn
                (set-variable 'py-indent-offset 4)
-               (set-variable 'py-smart-indentation nil)
+               (set-variable 'py-smart-indentation t)
                (set-variable 'indent-tabs-mode nil) )))
 
-;; (require 'jedi)
-;; (add-hook 'python-mode-hook 'jedi:setup)
-;; (setq jedi:setup-keys t)
-;; (setq jedi:complete-on-dot t)
+
+(eval-when-compile (require 'jedi nil t))
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
+(add-hook 'python-mode-hook 'jedi:setup)
+(add-hook 'python-mode-hook 'jedi:ac-setup)
+
+;; (when (require 'ipython nil t)
+;;   (setq-default py-shell-name "ipython")
+;;   (setq-default py-which-bufname "IPython")
+;;   (setq py-python-command-args '("--matplotlib" "--colors" "LightBG")))
+
+;; (setq ein:use-auto-complete t)
+;; (setq ein:use-auto-complete-superpack t)
+;; (setq ein:use-smartrep t))
+
+
+; don't split windows
+
+
 
 ;; (require 'elpy)
 ;; (setq elpy-rpc-backend "jedi")
 ;; (elpy-enable)
 ;; (elpy-clean-modeline)
-
-;; ;; (when (require 'ipython nil t)
-;; ;;   (setq py-python-command-args '("--matplotlib" "--colors" "LightBG"))
-;; ;;   (setq ein:use-auto-complete t)
-;; ;;   (setq ein:use-auto-complete-superpack t)
-;; ;;   (setq ein:use-smartrep t))
 
 ;; (require 'nose)
 ;; (defun nosetests-all-virtualenv ()
