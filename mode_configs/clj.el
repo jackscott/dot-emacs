fae5ac4de064af44115d2926e8583f0ea0b72d91
Jack Scott <jack@nine78.com>
2015-01-01 10:21:16 -0500

    giving clojure some love and breaking shit instead

--- mode_configs/clj.el
+++ mode_configs/clj.el
@@ -1,48 +1,55 @@
 (require 'namespaces)
+(require 'cider)
 (namespace clj
 	   :import [funcs]
-	   :packages [starter-kit-lisp cider clojure-mode
+	   :packages [starter-kit-lisp clojure-mode
                                        bug-reference-github
-                                       midje-mode
+                                       clojure-mode-extra-font-locking
+                                       clj-refactor
+                                       smartparens
+                                       ;midje-mode
                                        ;midje-test-mode
                                        ;clojure-jump-to-file
                                        ])
+;; handle CamelCase shit in java
+(add-hook 'clojure-mode-hook 'subword-mode)
 
-;; (require 'starter-kit-lisp)
-;; (require 'cider)
-;; (require 'clojure-mode)
-(require 'midje-mode)
-;; (require 'clojure-jump-to-file)
-(add-hook 'clojure-mode-hook 'midje-mode)
+;(add-hook 'clojure-mode-hook 'paredit-mode)
+(add-hook 'clojure-mode-hook 'smartparens-strict-mode)
 
 ;; docs are good
 (add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
 
-;; log output to *nrepl-messages*
-(setq nrepl-log-message t)
+;; ;; Set a bunch of truths 
+(funcs/set-list-items '(nrepl-log-message nrepl-hide-special-buffers
+                                          cider-prefer-local-resources
+                                          cider-repl-pop-to-buffer-on-connect
+                                          cider-repl-wrap-history)
+                      t)
 
-;; should hide the *nrepl-server* and *nrepl-connection* buffers
-(setq nrepl-hide-special-buffers t)
-
-(setq cider-prefer-local-resources t)
 (setq cider-repl-tab-command 'indent-for-tab-command)
-
-;; open up the new buffer once the connection is established
-(setq cider-repl-pop-to-buffer-on-connect t)
 
 (setq cider-show-error-buffer 'only-in-repl)
 (setq nrepl-buffer-name-separator "-")
 (setq cider-prompt-save-file-on-load nil)
 (setq cider-repl-result-prefix ";; => ")
-(setq cider-repl-wrap-history t)
-(setq cider-repl-history-size 10000)
-(setq cider-repl-history-file (emacsdir+ "cider-repl.clj"))
 
+(setq cider-repl-history-file (emacsdir+ "cider-repl.clj"))
 (add-hook 'cider-repl-mode-hook 'subword-mode)
 (add-hook 'cider-repl-mode-hook 'paredit-mode)
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 
+;; (map '(lambda (x) (add-hook 'cider-repl-mode-hook x))
+;;      '(subword-mode
+;;        paredit-mode
+;;        rainbow-delimiters-mode))
 
-;; make a keybinding for this
-;(use 'midje.repl) (autotest)
+;; ;; (setq '([cider-show-error-buffer only-in-repl]
+;; ;;         [nrepl-buffer-name-separator "-"]
+;; ;;         [cider-prompt-save-file-on-load nil]
+;; ;;         [cider-repl-result-prefix ";; => "]
+;; ;;         [cider-repl-history-size 10000]
+;; ;;         ))
 
+
+;; -
