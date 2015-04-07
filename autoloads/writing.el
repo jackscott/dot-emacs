40a39d0afcb01c2a90a6d37e917c2629477fb15f
Jack Scott <jack@nine78.com>
2015-04-07 00:40:48 -0400

    nothing really new, added the beginnings of an org-mode setup and a bunch of ccopy/paste jazz

---
+++ autoloads/writing.el
@@ -0,0 +1,5 @@
+
+
+;; (when (package-installed-p 'markdown-mode)
+;;   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . gfm-mode))
+;;   (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode)))
---
+++ mode_configs/myorg.el
@@ -0,0 +1,5 @@
+(require 'namespaces)
+(namespace org
+  :packages [org])
+
+(setq org-list-allow-alphabetical 1)
