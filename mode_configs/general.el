e8b07230c92867091dc70184de080348266aa8b3
Jack Scott <jack@nine78.com>
2015-10-05 11:16:56 -0400

    added yaml-mode and a minor mode for saltstack

--- mode_configs/general.el
+++ mode_configs/general.el
@@ -44,7 +44,9 @@
                       helm-ag
                       swiper
                       swiper-helm
-                      yafolding])
+                      yafolding
+                      yaml-mode
+                      ])
 
 
 ;;for some reason these dont work with :packages 
@@ -124,3 +126,11 @@
 ;;       (cons (cons git-commit-filename-regexp 'pop-to-buffer)
 ;;             (remove (cons git-commit-filename-regexp 'switch-to-buffer)
 ;;                     with-editor-server-window-alist)))
+
+
+(define-derived-mode saltstack-mode yaml-mode "Saltstack"
+  "Minimal Saltstack mode, based on `yaml-mode'."
+  (setq tab-width 2
+        indent-tabs-mode nil))
+
+(add-to-list 'auto-mode-alist '("\\.sls\\'" . saltstack-mode))
