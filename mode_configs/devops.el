6000f8fb7f0631528fcb9ac9afb3e7c79b24c5f6
Jack Scott <jack@nine78.com>
2017-09-13 08:27:00 -0400

    add terraform mode

--- mode_configs/devops.el
+++ mode_configs/devops.el
@@ -3,31 +3,12 @@
   :import [funcs my-py]
   :packages [mmm-mode
              yaml-mode
-             ;;salt-mode
-])
-
-
+             terraform-mode])
+             
 (require 'yaml-mode)
+(require 'terraform-mode)
 
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
 (setq yaml-indent-offset 2)
-(defn my-hook ()
-;;  (salt-mode 1)
-  (python-mode-hook)
-	)
-  
-;; (defn add-to-mode (mode lst)
-;;   (dolist (file lst)
-;;     (add-to-list 'auto-mode-alist
-;;                  (cons file mode))))
+(add-to-list 'auto-mode-alist
+            '("\\.yml\\'\\|.sls\\'\\|pillar\\.example\\'\\|\\.jinja\\'" . yaml-mode))
 
-;; (define-derived-mode saltstack-mode yaml-mode "Saltstack"
-;;   "Minimal Saltstack mode, based on `salt-mode'."
-;;   (setq tab-width 2
-;;         indent-tabs-mode nil))
-
-;;(add-to-list 'auto-mode-alist
-;;             '("\\.sls\\'\\|pillar\\.example\\'\\|\\.jinja\\'" . salt-mode))
-
-
-;;(add-hook 'salt-mode-hook (~ my-hook))
